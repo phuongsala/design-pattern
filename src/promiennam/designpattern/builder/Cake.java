6143c6f71512ccd431db1418cedb4cf38ac71954
phuong.nguyen <phuong.nguyen@mediastep.com>
2018-07-19 11:31:11 +0700

    * update Builder pattern

--- src/promiennam/designpattern/builder/Cake.java
+++ src/promiennam/designpattern/builder/Cake.java
@@ -1,84 +1,78 @@
 package promiennam.designpattern.builder;
 
 public class Cake {
-	
+
 	private Builder builder;
-	
-	public Cake(Builder builder){
+
+	public Cake(Builder builder) {
 		this.builder = builder;
 	}
-	
-	static class Builder{
-		
+
+	static class Builder {
+
 		private String name;
 		private int color;
 		private float width;
 		private float height;
 		private String factory;
 		private String producer;
-		
-		public Cake create(){
+
+		public Cake create() {
 			return new Cake(this);
 		}
-		
-		public Builder setName(String name){
+
+		public Builder setName(String name) {
 			this.name = name;
 			return this;
 		}
-		
-		public Builder setColor(int color){
+
+		public Builder setColor(int color) {
 			this.color = color;
 			return this;
 		}
-		
-		public Builder setWidth(float width){
+
+		public Builder setWidth(float width) {
 			this.width = width;
 			return this;
 		}
-		
-		public Builder setHeight(float height){
+
+		public Builder setHeight(float height) {
 			this.height = height;
 			return this;
 		}
-		
-		public Builder setFactory(String factory){
+
+		public Builder setFactory(String factory) {
 			this.factory = factory;
 			return this;
 		}
-		
-		public Builder setProducer(String producer){
+
+		public Builder setProducer(String producer) {
 			this.producer = producer;
 			return this;
 		}
-
-		public String getName() {
-			return name;
-		}
-
-		public int getColor() {
-			return color;
-		}
-
-		public float getWidth() {
-			return width;
-		}
-
-		public float getHeight() {
-			return height;
-		}
-
-		public String getFactory() {
-			return factory;
-		}
-
-		public String getProducer() {
-			return producer;
-		}
-		
 	}
 
-	public Builder getBuilder() {
-		return builder;
+	public String getName() {
+		return builder.name;
 	}
-	
+
+	public int getColor() {
+		return builder.color;
+	}
+
+	public String getProducer() {
+		return builder.producer;
+	}
+
+	public String getFactory() {
+		return builder.factory;
+	}
+
+	public float getWidth() {
+		return builder.width;
+	}
+
+	public float getHeight() {
+		return builder.height;
+	}
 }
--- src/promiennam/designpattern/builder/DEMO.java
+++ src/promiennam/designpattern/builder/DEMO.java
@@ -11,14 +11,14 @@ public class DEMO {
 				.setName("Muffin")
 				.setProducer("Nestle")
 				.create();
-		System.out.println("Cake's name is " + cake.getBuilder().getName());
-		System.out.println("Cake's producer is " + cake.getBuilder().getProducer());
+		System.out.println("Cake's name is " + cake.getName());
+		System.out.println("Cake's producer is " + cake.getProducer());
 
 		Cake cake2 = builder.setColor(0)
 				.setHeight(10)
 				.setWidth(20)
 				.create();
-		System.out.println("Cake 2 width = " + cake2.getBuilder().getWidth());
-		System.out.println("Cake 2 height = " + cake2.getBuilder().getHeight());
+		System.out.println("Cake 2 width = " + cake2.getWidth());
+		System.out.println("Cake 2 height = " + cake2.getHeight());
 	}
 }
